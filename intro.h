b3b09bd2ead39d2506170b370d501a20173ec907
Benay Tomas <115767219+luckynerd3@users.noreply.github.com>
2023-11-20 01:30:41 -0300

    Name Sorting

---
+++ intro.h
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<conio.h>
+#include<string.h>
+#include<time.h>
+#include<ctype.h>
+
+/*
+ 
+    // forma doida de scanear um numero inteiro
+
+    int t,i;
+    char line[16];
+    printf("Enter how many teachers are in department\n");
+    fgets(line, sizeof(line), stdin);  //armazena o numero em line
+    sscanf(line, "%d",&t);   // passa o valor de line como inteiro para t
+    for(i=1;i<=t;i++)   // usa o t como parametro
+
+*/
