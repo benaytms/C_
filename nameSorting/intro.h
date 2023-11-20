5f22dae8d921f25f1ecd3b5af806a3549ca8e018
Benay Tomas <115767219+luckynerd3@users.noreply.github.com>
2023-11-20 01:33:25 -0300

    Update intro.h

--- nameSorting/intro.h
+++ nameSorting/intro.h
@@ -12,7 +12,7 @@
 
     int t,i;
     char line[16];
-    printf("Enter how many teachers are in department\n");
+    printf("type the number\n");
     fgets(line, sizeof(line), stdin);  //armazena o numero em line
     sscanf(line, "%d",&t);   // passa o valor de line como inteiro para t
     for(i=1;i<=t;i++)   // usa o t como parametro
