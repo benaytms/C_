192ae0bdf0ffd428190dc45c90c022d6d9531de2
Benay Tomas <115767219+luckynerd3@users.noreply.github.com>
2023-12-01 21:36:23 -0300

    Update intro.h

--- nameSorting/intro.h
+++ nameSorting/intro.h
@@ -10,11 +10,9 @@
 //    e usando o nome como chave para ordenar - para isso usei os valores ASCII das letras
 
 typedef struct {
-
-    char nome[40];
-    char fone[20];
-    int ascVal;
-
+        char nome[40];
+        char fone[20];
+        int ascVal;
 } PESSOA;
 
 void agendaFilling (PESSOA agenda[], int *valorASCII, int n);
