4b776c422a4c712eacf4a67ac98cb8a73895f854
Benay Tomas <115767219+luckynerd3@users.noreply.github.com>
2023-12-01 22:24:05 -0300

    Update intro.h

--- partyGuest/intro.h
+++ partyGuest/intro.h
@@ -1,20 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
 #include<string.h>
-#include<time.h>
-#include<ctype.h>
 
 #define MAX_NAMES 5
 #define MAX_NAME_LENGTH 30
 
-// função para printar string
+// função para mostrar string
 void print_str(int v, char *n);
 
 // liberar memoria
 void liberaMem(char *n, char *m);
 
-// remover espaços de string
+// remover os espaços das string
 void removeSpacesFromStr(char **string, char *nome);
 
 // checa se o nome está na lista de convidados
